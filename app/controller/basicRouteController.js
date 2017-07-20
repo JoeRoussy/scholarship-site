f552a13e0f1ffb5398ed608d1a53929910f26197
Maddie <m.coelho>
2017-07-19 23:13:56 -0400

    Big style changes

--- app/controller/basicRouteController.js
+++ app/controller/basicRouteController.js
@@ -2,3 +2,15 @@
 exports.home = (req, res) => {
     res.render('home');
 };
+
+exports.aboutUs = (req, res) => {
+     res.render('about');
+}
+
+exports.result = (req, res) => {
+     res.render('result');
+}
+
+exports.contact = (req, res) => {
+     res.render('contact');
+}
