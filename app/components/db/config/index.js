1d25860dbb1f36229eabaefbe37ce54594552def
Joseph Roussy <joeroussy@gmail.com>
2017-08-06 20:30:19 -0400

    Fixed a comment

--- app/components/db/config/index.js
+++ app/components/db/config/index.js
@@ -1,7 +1,6 @@
 import { RuntimeError } from '../../custom-utils';
 import mongodb from 'mongodb';
 
-// Connect to mongodb and return the db connection if everything goes well.
 // Returns a promise that resolves into a DB connection
 export default () => {
     const MongoClient = mongodb.MongoClient;
