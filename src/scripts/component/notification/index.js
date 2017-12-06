4716fc6e85c965d16a33e98565b18f3e9aca32f2
Joseph Roussy <joeroussy@gmail.com>
2017-12-05 23:51:30 -0500

    Made the notification module more general

--- src/scripts/component/notification/index.js
+++ src/scripts/component/notification/index.js
@@ -1,19 +1,35 @@
 import iziToast from 'izitoast';
 
+const validTypes = [
+    'info',
+    'success',
+    'error',
+    'warning'
+];
+
 export default () => {
     $('[data-notification-trigger]').each((index, e) => {
         const element = $(e);
 
+        const type = element.attr('data-type');
         const title = element.attr('data-heading');
         const message = element.attr('data-body');
 
-        if (!(title && message)) {
+        if (!(title && message && type)) {
             console.error('Could not find title or heading for body for notification trigger', element);
 
             return;
         }
 
-        iziToast.success({
+        const isValidType = validTypes.some(x => x === type);
+
+        if (!isValidType) {
+            console.error(`Invalid type: ${type}`, element);
+
+            return;
+        }
+
+        iziToast[type]({
             title,
             message,
             position: 'topRight',
